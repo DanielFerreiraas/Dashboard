30f616a03c9171fdb65625f1fcb11fbe818ac28e
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-05-15 12:53:32 -0300

    ajustando api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"https://json-server-config.onrender.com",
+    baseURL:"https://json-server-ex-epu8.onrender.com/",
 })
 
 export default api;
